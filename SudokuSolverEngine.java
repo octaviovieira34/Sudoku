d5959f8db17fb5cd0a62099a9e344aa7503c9211
octavio :) <test@Tests-MacBook-Pro.local>
2025-12-09 18:34:25 -0800

    modified many things test

--- SudokuSolverEngine.java
+++ SudokuSolverEngine.java
@@ -1,7 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class SudokuSolverEngine {
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws FileNotFoundException {
       // Here I have called my class `MySudokuBoard` if you named your class
       // differently, modify the line below to use your own class name
       SudokuBoard board = new SudokuBoard("boards/very-fast-solve.sdk");
@@ -19,3 +21,38 @@ public class SudokuSolverEngine {
       
    }
 }
+
+ /* ----jGRASP exec: java SudokuSolverEngine
+ Initial board
+ . 3 4 | 6 7 8 | 9 1 2 
+ . 7 2 | 1 9 5 | 3 4 8 
+ 1 9 8 | 3 4 2 | 5 6 7 
+ ------+-------+------
+ . . 9 | . 6 1 | 4 2 3 
+ . 2 6 | 8 5 3 | 7 9 1 
+ . 1 3 | 9 2 4 | . 5 6 
+ ------+-------+------
+ . 6 1 | 5 3 7 | 2 8 4 
+ . 8 . | 4 1 9 | 6 3 5 
+ 3 4 5 | . 8 6 | 1 7 9 
+ 
+ 
+ Solving board...SOLVED in 0.003 seconds.
+ 
+ 5 3 4 | 6 7 8 | 9 1 2 
+ 6 7 2 | 1 9 5 | 3 4 8 
+ 1 9 8 | 3 4 2 | 5 6 7 
+ ------+-------+------
+ 8 5 9 | 7 6 1 | 4 2 3 
+ 4 2 6 | 8 5 3 | 7 9 1 
+ 7 1 3 | 9 2 4 | 8 5 6 
+ ------+-------+------
+ 9 6 1 | 5 3 7 | 2 8 4 
+ 2 8 7 | 4 1 9 | 6 3 5 
+ 3 4 5 | 2 8 6 | 1 7 9 
+ 
+ 
+  ----jGRASP: Operation complete. */\
+  
+  
+  // test test 
